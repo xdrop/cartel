3d7ab0f1dea1762f3f0667df21d0382b221fb831
xdrop <xdrop.me@gmail.com>
2021-06-12 22:42:35 +0100

    Update version bumping script
    Now also updates changelog and does the git commit

--- bump-version.sh
+++ bump-version.sh
@@ -2,8 +2,15 @@ NEW_VERSION=$1
 OLD_VERSION=$(cat VERSION)
 echo "Updating VERSION"
 echo $NEW_VERSION > VERSION
+echo "Updating CHANGELOG.md..."
+sed -i "s;## \[Unreleased\];## [Unreleased]\n\n## [$NEW_VERSION-beta] - $(date "+%Y-%m-%d");g" CHANGELOG.md
 echo "Updating Cargo.toml..."
 sed -i "s/^version = \"$OLD_VERSION\"/version = \"$NEW_VERSION\"/g" Cargo.toml
 echo "Trigger lockfile update..."
 touch src/client/cli.rs && cargo check
+echo "Committing.."
+git add 'Cargo.toml' 'Cargo.lock' 'CHANGELOG.md' 'VERSION'
+git commit -m "Bump to $NEW_VERSION (beta)"
+echo "Performing git tag..."
+git tag $NEW_VERSION-beta
 
