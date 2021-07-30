d9c2dacfde7afbb9411e4b7654a9a8e961dd00c2
xdrop <xdrop.me@gmail.com>
2021-07-30 18:37:32 +0300

    Switch emojis to correct width
    Some emojis render (incorrectly) as one space instead of two on some
    terminals. Swapping these out for more universality accross terminals.

--- src/client/emoji.rs
+++ src/client/emoji.rs
@@ -2,11 +2,10 @@ use console::Emoji;
 
 pub static LOOKING_GLASS: Emoji<'_, '_> = Emoji("🔍 ", " =>");
 pub static HOUR_GLASS: Emoji<'_, '_> = Emoji("⏳ ", " =>");
-pub static UP_ARROW: Emoji<'_, '_> = Emoji("⬆️  ", " =>");
 pub static SUCCESS: Emoji<'_, '_> = Emoji("✅ ", " =>");
 pub static NOTES: Emoji<'_, '_> = Emoji("📃 ", " =>");
 pub static TEXTBOOK: Emoji<'_, '_> = Emoji("📗 ", " =>");
 pub static LINK: Emoji<'_, '_> = Emoji("🔗 ", " =>");
 pub static VAN: Emoji<'_, '_> = Emoji("🚚 ", " =>");
-pub static SPIRAL_NOTEBOOK: Emoji<'_, '_> = Emoji("🗒️  ", " =>");
+pub static SPIRAL_NOTEBOOK: Emoji<'_, '_> = Emoji("📝 ", " =>");
 pub static YELLOW_NOTEBOOK: Emoji<'_, '_> = Emoji("📒️ ", " =>");
