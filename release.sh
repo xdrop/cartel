b7f1d5ac71d67f6422e802b024d8bf6bb147b334
xdrop <xdrop.me@gmail.com>
2025-03-16 02:45:13 +0000

    Update release script

--- release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cargo build --all --release
-mkdir -p target/latest 2>/dev/null
-mv target/release/client target/latest/client.darwin.amd64
-mv target/release/daemon target/latest/daemon.darwin.amd64
